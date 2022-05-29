77f1ed903cd9e1564e64380e98fb85bba50968b8
Srishti <sripor.62@gmail.com>
2022-05-29 19:31:49 +0530

    second

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Fourpro.wsgi --log-file -
---
+++ appFour/views.py
@@ -0,0 +1,67 @@
+from rest_framework import viewsets,status
+from .models import Movie,Rating
+from .serializers import MovieSerializer,RatingSerializer,UserSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.contrib.auth.models import User
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset=Movie.objects.all()
+    serializer_class=MovieSerializer
+    authentication_classes=(TokenAuthentication,)
+    permission_classes={IsAuthenticated,}
+
+    @action(detail=True,methods=['POST'])
+    def rate_movie(self,request,pk=None):
+        if 'stars' in request.data:
+            movie=Movie.objects.get(id=pk)
+            stars=request.data['stars']
+            user=request.user
+            print(user)
+
+
+            try:
+                rating=Rating.objects.get(user=user.id,movie=movie.id)
+                rating.star=stars
+                rating.save()
+                serializer=RatingSerializer(rating,many=False)
+
+                response={'message':"Rating updated",'result':serializer.data}
+                return Response(response,status=status.HTTP_200_OK)
+
+            except:
+                rating=Rating.objects.create(user=user,movie=movie,star=stars)
+                serializer=RatingSerializer(rating,many=False)
+
+                response={'message':"Rating created",'result':serializer.data}
+                return Response(response,status=status.HTTP_200_OK)
+
+
+        else:
+            response={'message':"It's not working"}
+            return Response(response,status=status.HTTP_400_BAD_REQUEST)
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset=Rating.objects.all()
+    serializer_class=RatingSerializer
+    authentication_classes=(TokenAuthentication,)
+    authentication_classes=(TokenAuthentication,)
+    permission_classes={IsAuthenticated,}
+
+    def update(self,request,*args,**kwargs):
+        response={'message':"You can't update rating"}
+        return Response(response,status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self,request,*args,**kwargs):
+        response={'message':"You can't create rating"}
+        return Response(response,status=status.HTTP_400_BAD_REQUEST)
